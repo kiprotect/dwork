accd1a47b3311aab92bd4fc05109faae63b173a6
Andreas Dewes <andreas.dewes@kiprotect.com>
2020-11-03 16:21:30 +0100

    Initial upload.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import find_packages
+from req import reqs
+
+setup(
+    name="dwork",
+    python_requires=">=3.6",
+    version="0.0.12",
+    author="KIProtect GmbH",
+    author_email="dwork@kiprotect.com",
+    license="BSD-3",
+    url="https://github.com/kiprotect/dwork",
+    packages=find_packages(),
+    package_data={"": ["*.ini"], "dwork" : ['py.typed']},
+    install_requires=reqs,
+    zip_safe=False,
+    # no console script so far
+    #entry_points={"console_scripts": ["dwork = dwork.cli.main:dwork"]},
+    description="A Python toolkit for differentially private data science & analytics.",
+)
