4dd77b8dd3ef46a9a3d8190473bb5f4c57391b37
Andreas Dewes <andreas.dewes@kiprotect.com>
2023-03-08 22:20:32 +0100

    v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from req import reqs
 setup(
     name="dwork",
     python_requires=">=3.6",
-    version="0.0.5",
+    version="0.0.6",
     author="KIProtect GmbH",
     author_email="dwork@kiprotect.com",
     license="BSD-3",
